0dce736c552d14c3f185ee0eba80bfb167c31014
Victor Pereira <victor_pereira@ifba.edu.br>
2020-04-10 16:47:41 -0300

    0.08 - legacy_fix

--- ffmpeg-m3u8-mp4.sh
+++ ffmpeg-m3u8-mp4.sh
@@ -12,10 +12,10 @@ url="" # This variable stores the file url
 line_count=0 # Content Line (Name, URL) Count
 conversion_count=0 # Number of files converted
 error_count=0
-alert_color=`tput setaf 3` # Yellow
-error_color=`tput setaf 1` # Red
-success_color=`tput setaf 2` # Green
-no_color=`tput sgr0` # No Color
+alert_color=$(tput setaf 3) # Yellow
+error_color=$(tput setaf 1) # Red
+success_color=$(tput setaf 2) # Green
+no_color=$(tput sgr0) # No Color
 output_path="" # This variable is a string that stores the output file path.
 output_folder="" # The output folder name for the next files is saved here
 output_file_name="" #The output file name
@@ -35,7 +35,7 @@ check_if_downloaded_file_exists(){
 		echo "$success_color""File Downloaded Successfully!""$no_color"
 	else
 		echo "$error_color""Error in Download! Please try another URL.""$no_color"
-		error_count=$(($error_count+1))
+		error_count=$((error_count+1))
 	fi
 }
 
@@ -43,7 +43,7 @@ savefile() { # Saves the file in the desired path
 
 	echo "Saving in: $output_path.mp4"
 	echo "Please wait..."
-	echo $(ffmpeg -y -i "$url" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "$output_path".mp4 -hide_banner -loglevel panic) # -loglevel panic This line is what makes all the magic happen :O
+	ffmpeg -y -i "$url" -bsf:a aac_adtstoasc -vcodec copy -c copy -crf 50 "$output_path".mp4 -hide_banner -loglevel panic # This line is what makes all the magic happen :O
 	check_if_downloaded_file_exists
 	echo "----------------------------"
 
@@ -57,10 +57,10 @@ read_lines_from_list_file() { # Reads each line of the list file and store it in
     
 	while IFS= read -r line; # Read each line of the given file and store in an array
     do
-        if [[ ! -z $line ]];
+        if [[ -n "$line" ]];
 		then
 			line_array[$line_count]="$line"
-			line_count=$(($line_count+1))
+			line_count=$((line_count+1))
 		fi
     done < "$listfile_name"
 
@@ -71,7 +71,7 @@ build_file_path(){ # Store the output file path string
 	parent_folder=$parent_folder_argument
 	output_path=""
 
-	if [[ ! -z "$parent_folder" ]]; # If the user sent a folder as an argument, it'll be used as a parent folder
+	if [[ -n "$parent_folder" ]]; # If the user sent a folder as an argument, it'll be used as a parent folder
 	then
 
 		output_path="$output_path""$parent_folder" # Add parent folder name to the path string
@@ -80,7 +80,7 @@ build_file_path(){ # Store the output file path string
 		
 	fi
 
-	if [[ ! -z "$output_folder" ]]; # If a folder was read inside the file, it'll be used as the child (output) folder
+	if [[ -n "$output_folder" ]]; # If a folder was read inside the file, it'll be used as the child (output) folder
 	then
 
 		output_path="$output_path""$output_folder" # Add output folder name to the path string		
@@ -113,20 +113,20 @@ convert() { # Method that triggers all the file conversion related methods
     for line in "${line_array[@]}"; #Read each line of the array
     do
 
-		if [[ ! -z "$line" ]]; # If a line has a non-empty string, work on it
+		if [[ -n "$line" ]]; # If a line has a non-empty string, work on it
 		then
 				
 			if [[ "$line" =~ ^"$folder_delimiter" ]]; # If a line has an output folder, save the new files in it
 			then
 
-				output_folder=$(echo $line | cut -f2 -d=)
+				output_folder=$(echo "$line" | cut -f2 -d=)
 				echo -e "$alert_color""FOLDER CHANGED TO $output_folder""$no_color"
 				conversion_count=0
 
 			elif [[ "$line"  =~ ^"$url_delimiter" ]]; # If the line count is odd (should contain an URL), save it to the previously read file name
 			then
 
-				conversion_count=$(($conversion_count+1))
+				conversion_count=$((conversion_count+1))
 				url="$line" # URL
 				build_file_path # Sets the output_file_path variable
 				savefile  # Saves the file
@@ -135,7 +135,7 @@ convert() { # Method that triggers all the file conversion related methods
 				output_file_name="$line"
 			fi
 			
-			line_count=$(($line_count+1)) #increment line count
+			line_count=$((line_count+1)) #increment line count
 
 		fi
     
@@ -153,10 +153,10 @@ then
 
 	for file in *.txt; do
 
-		echo "=========================================="
 		echo "$alert_color""PLAYLIST OPEN: ""$file""$no_color"
 		echo "=========================================="
 		convert "$file"
+		echo "=========================================="
 
 	done
 
@@ -171,7 +171,7 @@ then #Show finished message according to error count
 	echo "$success_color""Download finished! All files downloaded.""$no_color"
 else
 
-	echo "$warning_color""Download finished with warnings! ""$error_count "" tasks of ""$conversion_count"" failed.""$no_color"
+	echo "$alert_color""Download finished with warnings! ""$error_count "" tasks of ""$conversion_count"" failed.""$no_color"
 
 fi
 
@@ -183,10 +183,10 @@ then
 		echo -ne '\007'
 	elif [[ -z "$beep_option_argument" || "$beep_option_argument" == "-repeat" ]];
 	then
-		while [[ $1 == $1 ]]; 
+		while [[ "$1" == "$1" ]]; 
 		do
 			echo -ne '\007'
-			if [[ ! -z "$beep_interval_argument" ]];
+			if [[ -n "$beep_interval_argument" ]];
 			then
 				sleep "$beep_interval_argument"
 			else
